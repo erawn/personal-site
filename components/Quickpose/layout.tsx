8062b82705bb5216aa31306176f01ad59758a13a
Eric Rawn <erawn@berkeley.edu>
2023-01-19 22:24:37 -0800

    ga_tag fixes

--- components/Quickpose/layout.tsx
+++ components/Quickpose/layout.tsx
@@ -13,22 +13,6 @@ const Layout = ({ preview, subpage = "", children }: Props) => {
   return (
     <>
       <Meta subpage={subpage} />
-      <div className="container">
-        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID_PORTFOLIO}"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', process.env.GA_ID_PORTFOLIO);
-        `}
-        </Script>
-      </div>
       <div className="min-h-screen min-w-[415px]">
         {/* <Alert preview={preview} /> */}
         <main>{children}</main>
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,5 @@
-import Alert from "./alert";
 import Footer from "./footer";
 import Meta from "./meta";
-import Script from "next/script";
 type Props = {
   preview?: boolean;
   subpage?: string;
@@ -12,22 +10,6 @@ const Layout = ({ preview, subpage = "", children }: Props) => {
   return (
     <>
       <Meta subpage={subpage} />
-      <div className="container">
-        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID_PORTFOLIO}"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', process.env.GA_ID_PORTFOLIO);
-        `}
-        </Script>
-      </div>
       <div className="min-h-screen">
         {/* <Alert preview={preview} /> */}
         <main>{children}</main>
