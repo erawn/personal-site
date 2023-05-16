b07675c2cfa10d03433f5de0f354ef6a00c65f17
Eric Rawn <erawn@berkeley.edu>
2023-05-16 14:44:11 -0700

    update resume

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,5 @@
-export const EXAMPLE_PATH = 'blog-starter'
-export const CMS_NAME = 'Markdown'
-export const HOME_OG_IMAGE_URL =
-  '/assets/inkdrop.jpg'
+export const EXAMPLE_PATH = "blog-starter";
+export const CMS_NAME = "Markdown";
+export const HOME_OG_IMAGE_URL = "/assets/inkdrop.jpg";
 
-  export const RESUME_PATH = "/assets/resume_Oct2022.pdf"
+export const RESUME_PATH = "/assets/ResumeMay2023.pdf";
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,24 +1,23 @@
-import Container from '../components/container'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import Head from 'next/head'
-import Header from '../components/header'
-import React, { useState } from 'react';
-import { Document, Page } from 'react-pdf';
-import { setPDFWorker } from '../lib/api'
-import path from 'path';
-import fs from 'fs';
-import dynamic from 'next/dynamic'
-import Link from 'next/link'
-import { RESUME_PATH } from '../lib/constants'
-
+import Container from "../components/container";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import Head from "next/head";
+import Header from "../components/header";
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf";
+import { setPDFWorker } from "../lib/api";
+import path from "path";
+import fs from "fs";
+import dynamic from "next/dynamic";
+import Link from "next/link";
+import { RESUME_PATH } from "../lib/constants";
 
 export default function Index({ allPosts }) {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
-//https://codesandbox.io/s/react-pdf-next-js-forked-x12d2?file=/package.json
+  //https://codesandbox.io/s/react-pdf-next-js-forked-x12d2?file=/package.json
   const PDFViewer = dynamic(() => import("../components/pdf-viewer"), {
-    ssr: false
+    ssr: false,
   });
 
   return (
@@ -27,21 +26,21 @@ export default function Index({ allPosts }) {
         <Head>
           <title>Eric Rawn Portfolio</title>
         </Head>
-        <Header/>
+        <Header />
         <Container>
-     
-          <div className='flex flex-col justify-items-start'>
-           
-          <Link href={RESUME_PATH} className="text-red-600 text-2xl ml-4 mt-10 mb-0 text-center object-center">
+          <div className="flex flex-col justify-items-start">
+            <Link
+              href={RESUME_PATH}
+              className="text-red-600 text-2xl ml-4 mt-10 mb-0 text-center object-center"
+            >
               (Download PDF)
             </Link>
-          <div>
-            <PDFViewer pdfPath={RESUME_PATH}/>
-          </div>
+            <div>
+              <PDFViewer pdfPath={RESUME_PATH} />
+            </div>
           </div>
         </Container>
       </Layout>
     </>
-  )
+  );
 }
-
