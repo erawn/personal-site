4694b3ce66de55e7fde02dc940ff53f37e148357
Eric Rawn <erawn@berkeley.edu>
2025-03-19 17:21:02 -0700

    fix icons

--- components/intro.tsx
+++ components/intro.tsx
@@ -38,12 +38,12 @@ const Intro = () => {
               className="underline hover:text-blue-600 duration-200 transition-colors"
             >
               <span className="">
-              <Image className="w-3 whitespace-nowrap inline" unoptimized width={12} height={12} src={"/assets/icons/email.svg"} alt={"An Email Icon"}></Image>
-                
+                <Image className="w-3 whitespace-nowrap inline" unoptimized width={12} height={12} src={"/assets/icons/email.svg"} alt={"An Email Icon"}></Image>
+
               </span>
               erawn at berkeley dot edu
 
-            </a>. </p>
+            </a> </p>
 
 
             <p>
@@ -51,17 +51,17 @@ const Intro = () => {
                 href="https://scholar.google.com/citations?user=GupuwssAAAAJ&hl=en&oi=ao"
                 className="underline hover:text-blue-600 duration-200 transition-colors"
               >
-                
-                <span className="whitespace-nowrap">
+
+                /* <span className="whitespace-nowrap">
                   {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-3 whitespace-nowrap inline">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
                   </svg> */}
                   <Image className="w-3 whitespace-nowrap inline" unoptimized width={12} height={12} src={"/assets/icons/googlescholar.svg"} alt={"A Google Scholar Icon"}></Image>
                 </span>
-                
+
                 Google Scholar
 
-              </a>.
+              </a>
             </p>
 
             <p>
@@ -70,13 +70,13 @@ const Intro = () => {
                 className="underline hover:text-blue-600 duration-200 transition-colors"
               >
                 <span className="">
-                <Image className="w-3 whitespace-nowrap inline" unoptimized width={12} height={12} src={"/assets/icons/github.svg"} alt={"A Github Icon"}></Image>
-                 
+                  <Image className="w-3 whitespace-nowrap inline" unoptimized width={12} height={12} src={"/assets/icons/github.svg"} alt={"A Github Icon"}></Image>
+
 
                 </span>
                 Github
 
-              </a>.
+              </a>
             </p>
 
 
